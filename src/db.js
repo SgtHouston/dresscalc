60e44dec106680c8ff8a8594163cb7aed131171c
Chris Houston <chrisbcoding247@gmail.com>
2021-11-22 10:27:47 -0500

    added Variable Materials component

---
+++ src/db.js
@@ -0,0 +1,140 @@
+// Database
+
+export const db =
+
+{
+    questions: [
+        {
+            Explanation: "How Much Does The Fabric Cost? --->",
+            VMC: "Main Fabric Price Per Yard",
+            Value: "MainFabricPPY"
+        },
+        {
+            Explanation: "How Many Yards of this Fabric Will I Use? --->",
+            VMC: "# Yards Req",
+            Value: "NumMainYds"
+        },
+        {
+            Explanation:"How Much Does The Lining Fabric Cost Per Yard? --->",
+            VMC: "Lining Fabric Price Per Yard",
+            Value: "LiningFabricPPY"
+        },
+        {
+            Explanation:"How Many Yards of this Fabric Will I Use? --->",
+            VMC: "# Yards Req",
+            Value: "NumLiningYds"
+        },
+        {
+            Explanation:"How Much Does The Specialty Fabric Cost Per Yard? --->",
+            VMC: "Specialty Fabric Price Per Yard",
+            Value: " SpecialtyFabricPPY"
+        },
+        {
+            Explanation:"How Many Yards of this Fabric Will I Use? --->",
+            VMC: "# Yards Req",
+            Value: "NumSpecYds"
+        },
+        {
+            Explanation:"What Is The Local Retail Price Of The Crystals Pkg? --->",
+            VMC: "Crystals Price Per Pkg",
+            Value:"CrystalPPPkg"
+        },
+        {
+            Explanation: "How Many Packages At Retail Are Req? --->",
+            VMC: "# Req",
+            Value: "NumCrystalPkg"
+        },
+        {
+            Explanation:"How Much Does The Lace/Bead/Rhinestone Trim Costs Per Pkg? --->",
+            VMC: "Lace/Bead/Rhinestone Trim",
+            Value: "LaceBeadRhineTrim"
+        },
+        {
+            Explanation:"How Many Packages At Retail Are Req? --->",
+            VMC: "# Req",
+            Value: "NumLaceBeadRhineTrim"
+        },
+        {
+            Explanation:"How Much Do The Lace/Bead/Rhinestone Applique Costs Per Pkg? --->",
+            VMC: "Lace/Bead/Rhinestone Applique",
+            Value: "LaceBeadRhineApp"
+        },
+        {
+            Explanation:"How Many Packages At Retail Are Req? --->",
+            VMC: "# Req",
+            Value: "NumLaceBeadRhineApp"
+        },
+        {
+            Explanation:"How Much Do The Zippers/Buttons/Fasteners/Hooks Cost Per Pkg? --->",
+            VMC: "Zipper/Buttons/Fastener/Hook",
+            Value: "ZipperButtonHook"
+        },
+        {
+            Explanation:"How Many Packages At Retail Are Req? --->",
+            VMC: "# Req",
+            Value: "NumZipperButtonHook"
+        },
+        {
+            Explanation:"How Much Per Package At Retail --->",
+            VMC: "Horsehair",
+            Value: "Horsehair"
+        },
+        {
+            Explanation:"How Many Packages At Retail Are Req? --->",
+            VMC: "# Pkgs Req",
+            Value: "NumHorsehair"
+        },
+        {
+            Explanation:"How Much Per Package At Retail --->",
+            VMC: "Thread",
+            Value: "Thread"
+        },
+        {
+            Explanation:"How Many Packages At Retail Are Req? --->",
+            VMC: "# Req",
+            Value: "NumThread"
+        },
+        {
+            Explanation:"How Much Per Package At Retail --->",
+            VMC: "Embellishment #1",
+            Value: "Embellishment1"
+        },
+        {
+            Explanation:"How Many Packages At Retail Are Req? --->",
+            VMC: "# Req",
+            Value: "NumEmbellishment1"
+        },
+        {
+            Explanation:"How Much Per Package At Retail --->",
+            VMC: "Embellishment #2",
+            Value: "Embellishment2"
+        },
+        {
+            Explanation:"How Many Packages At Retail Are Req? --->",
+            VMC: "# Req",
+            Value: "NumEmbellishment2"
+        },
+        {
+            Explanation:"How Much Per Package At Retail --->",
+            VMC: "Embellishment #3",
+            Value: "Embellishment3"
+        },
+        {
+            Explanation:"How Many Packages At Retail Are Req? --->",
+            VMC: "# Req",
+            Value: "NumEmbellishment3"
+        },
+        {
+            Explanation:"How Much Per Package At Retail --->",
+            VMC: "Hang Tag",
+            Value: "HangTag"
+        },
+        {
+            Explanation:"How Many Hang Tags At Retail Are Req? --->",
+            VMC: "# Req",
+            Value: "NumHangTag"
+        },
+        
+    ]
+}
+
